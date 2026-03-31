a00bddb1a58d4242fd083240147613eeffa090e0
technobytedigital <technobyte.digital@gmail.com>
2026-03-31 19:06:15 +0530

    Update outreach.py

--- outreach.py
+++ outreach.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def generate_outreach(lead):
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
     prompt = f"""
 Write a short professional outreach message.
 
